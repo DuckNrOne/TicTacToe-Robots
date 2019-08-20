1a5fbe43bb228b6f0ff5e9124d0ae48ffb1166a9
Antony <tony.kamp@web.de>
2019-08-20 19:26:36 +0200

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -9,20 +9,20 @@ bool Human::chooseField(char * square){
     cin >> dec;
     dec--;
 
-    if ((dec == 0 && *(square+dec) == '1') ||
-        (dec == 1 && *(square+dec) == '2')  ||
-        (dec == 2 && *(square+dec) == '3')  ||
-        (dec == 3 && *(square+dec) == '4')  ||
-        (dec == 4 && *(square+dec) == '5')  ||
-        (dec == 5 && *(square+dec) == '6')  ||
-        (dec == 6 && *(square+dec) == '7')  ||
-        (dec == 7 && *(square+dec) == '8')  ||
-        (dec == 8 && *(square+dec) == '9')){
+    if ((dec == 0 && square[dec] == '1') ||
+        (dec == 1 && square[dec] == '2')  ||
+        (dec == 2 && square[dec] == '3')  ||
+        (dec == 3 && square[dec] == '4')  ||
+        (dec == 4 && square[dec] == '5')  ||
+        (dec == 5 && square[dec] == '6')  ||
+        (dec == 6 && square[dec] == '7')  ||
+        (dec == 7 && square[dec] == '8')  ||
+        (dec == 8 && square[dec] == '9')){
         
-        *(square+dec) = getIcon();
+        square[dec] = getIcon();
         return true;
 
-    } else if ((*(square+dec) == 'X') || (*(square+dec) == 'O') || (dec > 8) || (dec < 1)){
+    } else if ((square[dec] == 'X') || (square[dec] == 'O') || (dec > 8) || (dec < 1)){
         cout << "Wrong entery. ";
         system("pause");
         return false;
